340d86e0d65d3b1fe00b0c6733eb9f81ebc6784f
bianwenyuan <bianwy23@gmail.com>
2026-02-11 11:41:07 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartImpl.java
@@ -74,4 +74,14 @@ public class ShoppingCartImpl implements ShoppingCartService {
             }
 
     }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long currentId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(currentId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return  list;
+    }
 }
